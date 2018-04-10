65bbbd2cd727d2c16e8643c4cf6680f90459d177
Khalid Hosein <khalid.hosein9@gmail.com>
2018-04-10 11:05:01 -0400

    Finished CRUD for trips

--- client/src/services.js
+++ client/src/services.js
@@ -3,13 +3,10 @@ import axios from 'axios';
 class Services {
 
 	getAllTrips() {
-		
 		return axios.get('/api/trips')
-
 	};
 
 	getOneTrip(id) {
-
 		console.log("inside services")
 		return axios.get(`/api/trips/${id}`);
 	
@@ -25,13 +22,33 @@ class Services {
 				date_of_arr: trip.date_of_arr,
 				start_city: trip.start_city,
 				end_city: trip.end_city,
-				details: trip.detailsr
+				details: trip.details
 
 			}
 		});
 	};
 
+	updateTrip(trip, id) {
+		return axios({
+			method: "PUT",
+			url: `/api/trips/${id}`,
+			data: {
+				description: trip.description,
+				date_of_dep: trip.date_of_dep,
+				date_of_arr: trip.date_of_arr,
+				start_city: trip.start_city,
+				end_city: trip.end_city,
+				details: trip.details
+			}
+		})
+	}
 
+	deleteTrip(id) {
+		return axios({
+			method: "DELETE",
+			url: `/api/trips/${id}`
+		})
+	}
 };
 
 export default new Services();
